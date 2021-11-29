ab7ad5c4163b2340d8a1761fa69774f968a7c5ae
naveen-murali <naveenmurali1211@gmail.com>
2021-11-29 15:59:40 +0530

    all the validation msg is changed in to bottom, Everything will now on display at the bottom of the input.

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,78 +1,65 @@
 
-    $("#contact-form").submit((e) => {
-      e.preventDefault();
-      let formData = $("#contact-form").serializeArray();
-      let alerts_details = [];
-      let alerts = "";
-
-      formData.forEach(({ name, value }) => {
-        if (value == false)
-          alerts_details.push(`"${name.charAt(0).toUpperCase() + name.slice(1)}" field is empty.`);
-
-        if (value && name == "email" && !isEmail(value))
-          alerts_details.push("Please enter a valid email address");
-      });
-
-      alerts_details.forEach((value) => {
-        alerts += `
-          <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <em>${value}</em>
+$("#contact-form").submit((e) => {
+  e.preventDefault();
+  let formData = $("#contact-form").serializeArray();
+  let alerts = true;
+  formData.forEach(({ name, value }) => {
+    if (value && name == "email" && !isEmail(value)){
+      $(`#${name}`).html(`<p class='text-danger' >Please enter a valid email address</p>`)
+      alerts = false;
+    }
+    if (value == false){
+      $(`#${name}`).html(`<p class='text-danger'>${name.charAt(0).toUpperCase() + name.slice(1)} can not be empty</p>`);
+      alerts = false;
+    }
+  });
+  
+  if (alerts == true) {
+    $("#main-alert").html(`
+          <div class="spinner-grow spinner-grow-sm text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div class="spinner-grow spinner-grow-sm text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div class="spinner-grow spinner-grow-sm text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>`);
+    $.ajax({
+      type: "post",
+      url: "https://script.google.com/macros/s/AKfycbx_kA9qYtsPCifMHioiJfKaoBpyYtB8besGmRGXrb3kC6LJ5vMr8tabm07lQT4vls-u/exec",
+      data: $("#contact-form").serialize(),
+      success: function (response) {
+        $("#main-alert").html(`
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Details Successfully submitted</strong>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
-          </div>`;
-      });
-
-      if (alerts) {
-        $("#main-alert").html(alerts);
-        alerts = "";
-      } else {
+          </div>
+        `);
+        $("#contact-form").trigger('reset');
+      },
+      error:function (error){              
         $("#main-alert").html(`
-              <div class="spinner-grow spinner-grow-sm text-danger" role="status">
-                <span class="sr-only">Loading...</span>
-              </div>
-              <div class="spinner-grow spinner-grow-sm text-danger" role="status">
-                <span class="sr-only">Loading...</span>
-              </div>
-              <div class="spinner-grow spinner-grow-sm text-danger" role="status">
-                <span class="sr-only">Loading...</span>
-              </div>`);
-        $.ajax({
-          type: "post",
-          url: "https://script.google.com/macros/s/AKfycbx_kA9qYtsPCifMHioiJfKaoBpyYtB8besGmRGXrb3kC6LJ5vMr8tabm07lQT4vls-u/exec",
-          data: $("#contact-form").serialize(),
-          success: function (response) {
-            $("#main-alert").html(`
-              <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong>Details Successfully submitted</strong>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            `);
-            $("#contact-form").trigger('reset');
-          },
-          error:function (error){              
-            $("#main-alert").html(`
-              <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <em>Submission failed.</em>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            `);
-          }
-        });
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <em>Submission failed.</em>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        `);
       }
+    });
+  }
+})
 
-    })
-
-$("#email").on("input", (e)=>{
-    if(!isEmail(e.target.value)){
-        $("#phoneDiv").html("<p class='text-danger' >Enter valid email.</p>");
+$("#emailInput").on("input", (e)=>{
+    if(!isEmail(e.target.value) || e.target.value == ""){
+        $("#email").html("<p class='text-danger' >Enter valid email.</p>");
     }
-    if(isEmail(e.target.value) || ! e.target.value){
-        $("#phoneDiv").html("");
+    if(isEmail(e.target.value)){
+        $("#email").html("");
     }
 });
 
@@ -100,4 +87,11 @@ function noSpace(evt){
     if (ASCIICode == 32)
         return false;
     return true;
+}
+
+function checkEmpty(id, self){
+  if(self.value == false)
+    $(`#${id}`).html(`<p class='text-danger'>${id.charAt(0).toUpperCase() + id.slice(1)} can not be empty</p>`);
+  else
+    $(`#${id}`).html("");
 }
